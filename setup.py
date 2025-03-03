3fe681f1979cc4dd779a2f9d3845994fa0954fdc
romainsacchi <r_s@me.com>
2025-03-03 23:32:40 +0100

    fix test

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     license=open("LICENSE").read(),
     package_data={"carculator": package_files(os.path.join("carculator", "data"))},
     install_requires=[
-        "carculator_utils>=1.3.0",
+        "carculator_utils>=1.3.1",
     ],
     url="https://github.com/romainsacchi/carculator",
     description="Prospective life cycle assessment of vehicles made blazing fast",
