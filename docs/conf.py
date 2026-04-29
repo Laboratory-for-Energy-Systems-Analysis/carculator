9a42db8a1c6459d13729abd8140ca4fb57a7e4eb
romainsacchi <r_s@me.com>
2026-04-29 14:57:14 +0200

    fix: align package metadata

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = "2019, Paul Scherrer Institut"
 author = "Chris Mutel, Brian Cox, Romain Sacchi"
 
 # The full version, including alpha/beta/rc tags
-release = "1.6.8"
+release = "1.9.4"
 
 
 # -- General configuration ---------------------------------------------------
